a14e6dfd640150ead4a112c5cb0ecf152f8dad12
Bernardo Falé <mbfale@ua.pt>
2023-11-11 22:04:22 +0000

    Adding exercises

---
+++ clippy/target/debug/deps/clippy3-5874c80063916c37.d
@@ -0,0 +1,8 @@
+/Users/bernardofale/Documents/rustlings/exercises/clippy/target/debug/deps/clippy3-5874c80063916c37.rmeta: clippy3.rs Cargo.toml
+
+/Users/bernardofale/Documents/rustlings/exercises/clippy/target/debug/deps/clippy3-5874c80063916c37.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
